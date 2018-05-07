5c6cb4c56d226db0e7dc25ef14527dab7123ff39
santi <santi@192.168.1.3>
2018-05-07 20:53:40 +0200

    Rama

--- GitASubir/src/GitSubir.java
+++ GitASubir/src/GitSubir.java
@@ -4,6 +4,7 @@ public class GitSubir {
 
 	public static void main(String[] args) {
 		System.out.println("Este es el proyecto de Git que vamos a subir a GitHub");
+		System.out.println("Esta es la nueva rama modificada");
 	}
 
 }		
